1aa95cfa9ce8277445057a70bae39a33b97c9d43
DecoMay <143063276+DecoMay@users.noreply.github.com>
2023-09-23 23:28:29 +0800

    Add gridRes function

--- scripts.js
+++ scripts.js
@@ -2,21 +2,25 @@ const div = document.querySelector('.container');
 const BASE_CANVAS_SIZE = 600;
 const clearButton = document.querySelector('#clear');
 const changeRes = document.querySelector('#changeRes');
+const colorButton = document.querySelector('#colorMode');
+let colorMode = 'black';
 let gridRes = 10;
 
 function createGrid() {
-    while (div.firstChild) {
-        div.removeChild(div.lastChild);
-    }
-
-    for (let i = 0; i < (gridRes * gridRes); i++) {
-        let gridDiv = document.createElement('div');
-        // gridDiv.textContent = `${i}`;
-        gridDiv.style.width = `${(BASE_CANVAS_SIZE / gridRes)}px`;
-        gridDiv.style.height = `${(BASE_CANVAS_SIZE / gridRes)}px`;
-        gridDiv.classList.add('grid');
-        gridDiv.addEventListener('mouseenter', changeColor);
-        div.appendChild(gridDiv);
+    if (!isNaN(gridRes)) {
+        while (div.firstChild) {
+            div.removeChild(div.lastChild);
+        }
+        for (let i = 0; i < (gridRes * gridRes); i++) {
+            let gridDiv = document.createElement('div');
+            gridDiv.style.width = `${(BASE_CANVAS_SIZE / gridRes)}px`;
+            gridDiv.style.height = `${(BASE_CANVAS_SIZE / gridRes)}px`;
+            gridDiv.classList.add('grid');
+            gridDiv.addEventListener('mouseenter', changeColor);
+            div.appendChild(gridDiv);
+        }
+    } else {
+        return;
     }
 }
 
@@ -31,16 +35,30 @@ function randomColor() {
 
 function changeColor(e) {
     let currentGrid = e.target;
-    // if (currentGrid)
-    currentGrid.style.background = randomColor();
+    if (colorMode === 'color') {
+        currentGrid.style.background = randomColor();
+    } else {
+        currentGrid.style.background = 'black';
+        // currentGrid.style.border = '1px solid white';
+    }
     currentGrid.classList.add('colored');
-    // currentGrid.style.color = 'white';
     console.log(e.target.className == 'grid colored');
 }
 
 createGrid();
 clearButton.addEventListener('click', createGrid);
-// changeRes.addEventListener('click', () => {
-    // gridRes = 30;
-    // createGrid();
-// });
+colorButton.addEventListener('click', () => {
+    if (colorMode === 'black') {
+        colorMode = 'color';
+        colorButton.style.background = randomColor();
+    } else {
+        colorMode = 'black';
+        colorButton.style.background = 'black';
+        colorButton.style.color = 'white';
+    }
+    createGrid();
+});
+changeRes.addEventListener('click', () => {
+    gridRes = Number(prompt('1-100'));
+    createGrid();
+})
