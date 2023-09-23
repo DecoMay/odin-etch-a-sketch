a8e0042752e6dccc4b313450a4dd924893d126ed
DecoMay <143063276+DecoMay@users.noreply.github.com>
2023-09-23 21:51:01 +0800

    Add code for width and height

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
 const div = document.querySelector('.container');
+const CANVAS_SIZE = 600;
+
 for (let i = 0; i < (16 * 16); i++) {
     let gridDiv = document.createElement('div');
     gridDiv.textContent = `${i}`;
+    gridDiv.style.width = `${(600 / 16)}px`;
+    gridDiv.style.height = `${(600 / 16)}px`;
     gridDiv.classList.add('grid');
     div.appendChild(gridDiv);
 }
